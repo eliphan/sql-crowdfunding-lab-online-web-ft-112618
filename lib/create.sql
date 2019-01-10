b050be7be5ef4488177b7db19929355b5af98a40
Elise Phan <aliceconnic@gmail.com>
2019-01-10 04:20:24 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,6 +2,6 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY, 
     title TEXT, 
     category TEXT, 
-    funding_goal TEXT
-    start_date TEXT
+    funding_goal TEXT,
+    start_date TEXT,
     end_date TEXT);
