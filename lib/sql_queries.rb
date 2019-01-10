2212f1cba17574f3d0b5d05da509122f00b8b978
Elise Phan <aliceconnic@gmail.com>
2019-01-10 04:40:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,8 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title
-  FROM projects JOIN pledges.amount 
+  FROM projects 
+  JOIN pledges.amount 
   ON project.id = pledges.project_id"
 end
 
