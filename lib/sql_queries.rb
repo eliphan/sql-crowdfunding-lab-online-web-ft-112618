774dfeba52c6a391ffca59905e59588ddb9c1ea8
Elise Phan <aliceconnic@gmail.com>
2019-01-10 05:15:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
  FROM users
  JOIN pledges
  ON user.id = pledges.user_id
- GROUP BY user.name"
+ GROUP BY user.
+ ORDER BY pledges.amount DESC, user.name DESC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
